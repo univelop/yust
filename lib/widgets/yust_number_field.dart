24e01745065018f3845fb0a91e5974e0fcfb0337
Jannes Köhler <janneskohler@JannesMacBookTraser-2.local>
2020-05-26 23:06:30 +0200

    Add number field

---
+++ lib/widgets/yust_number_field.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:yust/yust.dart';
+
+typedef ChangeCallback = void Function(num);
+typedef TabCallback = void Function();
+
+class YustNumberField extends StatefulWidget {
+  final String label;
+  final num value;
+  final ChangeCallback onChanged;
+  final TabCallback onTab;
+  final bool readOnly;
+  final bool enabled;
+  final YustInputStyle style;
+
+  YustNumberField({
+    Key key,
+    this.label,
+    this.value,
+    this.onChanged,
+    this.onTab,
+    this.enabled = true,
+    this.readOnly = false,
+    this.style,
+  }) : super(key: key);
+
+  @override
+  _YustNumberFieldState createState() => _YustNumberFieldState();
+}
+
+class _YustNumberFieldState extends State<YustNumberField> {
+  TextEditingController _controller;
+  num oldValue;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = TextEditingController(text: widget.value?.toString());
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_controller.text != widget.value?.toString()) {
+      _controller.text = widget.value?.toString();
+    }
+    return TextField(
+      decoration: InputDecoration(
+          labelText: widget.label,
+          contentPadding: const EdgeInsets.all(20.0),
+          border: widget.style == YustInputStyle.outlineBorder
+              ? OutlineInputBorder()
+              : null),
+      controller: _controller,
+      onChanged: (value) {
+        if (value == '') {
+          widget.onChanged(null);
+        } else if (!['.', ','].contains(value.substring(value.length - 1))) {
+          value = value.replaceAll(RegExp(r'\,'), '.');
+          final numValue = num.tryParse(value);
+          if (numValue != null && numValue != oldValue) {
+            setState(() {
+              oldValue = numValue;
+            });
+            widget.onChanged(numValue);
+          }
+        }
+      },
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
+      inputFormatters: [WhitelistingTextInputFormatter(RegExp("[0-9\,\.]"))],
+      onTap: widget.onTab,
+      readOnly: widget.readOnly,
+      enabled: widget.enabled,
+    );
+  }
+}

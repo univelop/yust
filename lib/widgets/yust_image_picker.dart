1584dce47852a49bd2f109e11b1d26b7311afe89
Jannes Koehler <janneskoehler@gmail.com>
2020-09-24 11:09:28 +0200

    Remove web dependencies

--- lib/widgets/yust_image_picker.dart
+++ lib/widgets/yust_image_picker.dart
@@ -1,13 +1,10 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:mime/mime.dart';
-import 'package:yust/models/yust_exception.dart';
 import 'package:yust/screens/image.dart';
 import 'package:yust/yust.dart';
 

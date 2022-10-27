6a97825b40a60fafc6730e97216152d430938f8b
Jannis Rosenbaum <j.ro@dock51.de>
2022-10-27 13:15:18 +0200

    Change toIso8601StringWithOffset

--- lib/src/extensions/date_time_extension.dart
+++ lib/src/extensions/date_time_extension.dart
@@ -5,11 +5,14 @@ extension DateTimeExtension on DateTime {
     if (isUtc) {
       return isoDate;
     } else {
-      final hours = timeZoneOffset.inHours;
-      var offset = hours < 0 ? '-' : '+';
-      offset +=
-          '${_twoDigits(hours)}:${_twoDigits(timeZoneOffset.inMinutes % hours)}';
-      return isoDate + offset;
+      // Because dart only knows the utc timezone and the local timezone,
+      // we convert the date to UTC. The toIso8601String will automatically append a 'Z',
+      // to signify the date string is a UTC date.
+      // We can't use something like timeZoneOffset, because this doesn't get populated
+      // for parsed dates (only locally created dates). And because we don't know if
+      // other clients like the backend are in the same local time,
+      // UTC is the only option.
+      return toUtc().toIso8601String();
     }
   }
 

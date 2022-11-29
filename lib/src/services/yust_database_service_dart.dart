1beb0798b5558515ae70ce4d688f790905ad1018
Jannis Rosenbaum <j.ro@dock51.de>
2022-11-29 20:11:09 +0100

    Add getDocsChunked

--- lib/src/services/yust_database_service_dart.dart
+++ lib/src/services/yust_database_service_dart.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:googleapis/firestore/v1.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 import '../extensions/date_time_extension.dart';
 import '../extensions/string_extension.dart';
@@ -100,6 +103,67 @@ class YustDatabaseService {
         })
         .whereType<T>()
         .toList();
+  }
+
+  /// Returns [YustDoc]s as a lazy, chunked Stream from the database.
+  ///
+  /// This is much more memory efficient in comparison to other methods,
+  /// because of three reasons:
+  /// 1. It gets the data in multiple requests ([pageSize] each); the raw json
+  ///    strings and raw maps are only in memory while one chunk is processed.
+  /// 2. It loads the records *lazily*, meaning only one chunk is in memory while
+  ///    the records worked with (e.g. via a `await for(...)`)
+  /// 3. It doesn't use the google_apis package for the request, because that
+  ///    has a huge memory leak
+  ///
+  /// [docSetup] is used to read the collection path.
+  ///
+  /// [filters] each entry represents a condition that has to be met.
+  /// All of those conditions must be true for each returned entry.
+  ///
+  /// Consists at first of the column name followed by either 'ASC' or 'DESC'.
+  /// Multiple of those entries can be repeated.
+  ///
+  Stream<T> getDocsChunked<T extends YustDoc>(
+    YustDocSetup<T> docSetup, {
+    List<YustFilter>? filters,
+    List<String>? orderByList,
+    int pageSize = 5000,
+  }) {
+    final parent = _getParentPath(docSetup);
+    final url =
+        '${YustFirestoreApi.rootUrl}v1/${Uri.encodeFull(parent)}:runQuery';
+
+    Stream<Map<dynamic, dynamic>> lazyPaginationGenerator() async* {
+      var isDone = false;
+      var lastOffset = 0;
+      while (!isDone) {
+        final request = _getQuery(docSetup,
+            filters: filters,
+            orderByList: orderByList,
+            limit: pageSize,
+            offset: lastOffset);
+        final body = jsonEncode(request);
+        final result = await YustFirestoreApi.httpClient?.post(
+          Uri.parse(url),
+          body: body,
+        );
+        if (result == null) return;
+
+        final response =
+            List<Map<dynamic, dynamic>>.from(jsonDecode(result.body));
+
+        isDone = response.length < pageSize;
+        if (!isDone) lastOffset += pageSize;
+
+        yield* Stream.fromIterable(response);
+      }
+    }
+
+    return lazyPaginationGenerator().map<T?>((e) {
+      if (e['document'] == null) return null;
+      return _transformDoc<T>(docSetup, Document.fromJson(e['document']));
+    }).whereType<T>();
   }
 
   /// Returns a stream of a [YustDoc].
@@ -365,6 +429,7 @@ class YustDatabaseService {
     List<YustFilter>? filters,
     List<String>? orderByList,
     int? limit,
+    int? offset,
   }) {
     return RunQueryRequest(
       structuredQuery: StructuredQuery(
@@ -375,7 +440,8 @@ class YustDatabaseService {
                       _executeFilters(filters),
                   op: 'AND')),
           orderBy: _executeOrderByList(orderByList),
-          limit: limit),
+          limit: limit,
+          offset: offset),
     );
   }
 

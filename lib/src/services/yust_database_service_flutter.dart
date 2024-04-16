e960fc1013403e23a97ee3f629ef6168e3b40a6f
LeonHermann322 <Leon.Hermann@student.hpi.uni-potsdam.de>
2024-04-16 09:42:35 +0200

    #2872 Tries to update if doNotCreate is set

--- lib/src/services/yust_database_service_flutter.dart
+++ lib/src/services/yust_database_service_flutter.dart
@@ -359,10 +359,17 @@ class YustDatabaseService {
       jsonDoc,
       removeNullValues: removeNullValues ?? docSetup.removeNullValues,
     );
-    if (doNotCreate && (await get(docSetup, doc.id)) == null) return;
-    await collection
-        .doc(doc.id)
-        .set(modifiedDoc, SetOptions(merge: merge, mergeFields: updateMask));
+    if (doNotCreate) {
+      try {
+        await collection.doc(doc.id).update(modifiedDoc);
+      } catch (_) {
+        return;
+      }
+    } else {
+      await collection
+          .doc(doc.id)
+          .set(modifiedDoc, SetOptions(merge: merge, mergeFields: updateMask));
+    }
     if (!skipLog) {
       dbLogCallback?.call(
           DatabaseLogAction.save, _getCollectionPath(docSetup), 1,

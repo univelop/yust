2b0b8d78710ac122bfc6b2d16f580f368c476977
Jannik <44322559+cJannik@users.noreply.github.com>
2024-12-05 11:55:17 +0100

    #4278 fix mocked function for nested update mask keys

--- lib/src/services/yust_database_service_mocked.dart
+++ lib/src/services/yust_database_service_mocked.dart
@@ -458,6 +458,9 @@ class YustDatabaseServiceMocked extends YustDatabaseService {
     final segments = path.split('.');
     Map subDoc = jsonDoc;
     for (final segment in segments.sublist(0, segments.length - 1)) {
+      if (subDoc[segment] == null || subDoc[segment] is! Map<String, dynamic>) {
+        subDoc[segment] = <String, dynamic>{};
+      }
       subDoc = subDoc[segment];
     }
     subDoc[segments.last] = newValue;
